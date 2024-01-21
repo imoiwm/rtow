18cb7d9551e924b93c1268332aa984a1f0f57539
imoiwm <imoiwm@gmail.com>
2024-01-21 05:53:07 -0500

    finished thru chapter 5- made a raytraced sphere!

--- ch2/main.cc
+++ ch2/main.cc
@@ -1,18 +1,60 @@
 #include "color.h"
+#include "ray.h"
 #include "vec3.h"
 
 #include <iostream>
 
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = r.origin() - center;
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2 * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b * b - 4 * a * c;
+    return (discriminant >= 0);
+}
+
+color ray_color(const ray& r) {
+    if (hit_sphere(point3(0, 0, 1), 0.5, r)) {
+        return color(1, 0, 0);
+    }
+    vec3 unit_direction = unit_vector(r.direction());
+    auto a = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+}
+
 int main() {
-    int img_width = 256;
-    int img_height = 256;
+    
+    auto aspect_ratio = 16.0 / 9.0;
+    int img_width = 400;
+
+    int img_height = static_cast<int> (img_width / aspect_ratio);
+    img_height = (img_height < 1) ? 1 : img_height;
+
+    auto focal_length = 1.0;
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * (static_cast<double>(img_width)/img_height);
+    auto camera_center = point3(0, 0, 0);
+
+    auto viewport_u = vec3(viewport_width, 0, 0);
+    auto viewport_v = vec3(0, -viewport_height, 0);
+
+    auto pixel_delta_u = viewport_u / img_width;
+    auto pixel_delta_v = viewport_v / img_height;
+
+    auto viewport_upper_left = camera_center
+                               - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
+
+    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
     std::cout << "P3\n" << img_width << ' ' << img_height << "\n255\n";
 
     for (int j = 0; j < img_height; ++j) {
         std::clog << "\rScanlines remaining: " << (img_height - j) << ' ' << std::flush;
         for (int i = 0; i < img_width; ++i) {
-            auto pixel_color = color(double(i) / (img_width - 1), double(j) / (img_height - 1), 0);
+            auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+            auto ray_direction = pixel_center - camera_center;
+            ray r(camera_center, ray_direction);
+            color pixel_color = ray_color(r);
             write_color(std::cout, pixel_color);
         }
     }
