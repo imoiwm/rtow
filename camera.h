86673b254415e1e6d9dccc862d8e2c2542a18d5c
imoiwm <imoiwm@gmail.com>
2024-05-30 03:09:57 -0400

    completed thru 9.3

--- camera.h
+++ camera.h
@@ -15,6 +15,7 @@ class camera {
         double aspect_ratio = 1.0; // Ration of image width over height
         int img_width = 100; // Rendered image width in pixel count
         int samples_per_pixel = 10; // Count of random samples for each pixel
+        int max_depth = 10; // Maximum number of ray bounces into scene
 
         void render(const hittable& world) {
             initialize();
@@ -27,7 +28,7 @@ class camera {
                     color pixel_color(0, 0, 0);
                     for (int sample = 0; sample < samples_per_pixel; ++sample) {
                         ray r = get_ray(i, j);
-                        pixel_color += ray_color(r, world);
+                        pixel_color += ray_color(r, max_depth, world);
                     }
                     write_color(std::cout, pixel_color, samples_per_pixel);
                 }
@@ -86,12 +87,15 @@ class camera {
             return (px * pixel_delta_u) + (py * pixel_delta_v);
         }
 
-        color ray_color(const ray& r, const hittable& world) const {
+        color ray_color(const ray& r, int depth, const hittable& world) const {
+            if (depth <= 0) {
+                return color(0, 0, 0);
+            }
             hit_record rec;
 
-            if (world.hit(r, interval(0, infinity), rec)) {
+            if (world.hit(r, interval(0.001, infinity), rec)) {
                 vec3 direction = random_on_hemisphere(rec.normal);
-                return 0.5 * ray_color(ray(rec.p, direction), world);
+                return 0.5 * ray_color(ray(rec.p, direction), depth - 1, world);
             }
 
             vec3 unit_direction = unit_vector(r.direction());
